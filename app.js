40938c1f28dd6c46dff34f99850c123ce400311a
vass <sir.lagalot.thegreat@gmail.com>
2013-09-02 17:45:31 +0400

    [evan-dev2] initial commit express/backbone routes

--- app.js
+++ app.js
@@ -5,62 +5,38 @@
 
 var express = require('express')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , APPCONFIG = require('config').app;
 
 var app = express();
 
-var config = require('./appConfig')
-  , appVer = require('./appStatic');
-
-
 // all environments
-app.set('port', config.appPort);
-app.set('views', __dirname + '/views');
-//app.set('view engine', 'jade');
-//app.use(express.favicon());
-//app.use(express.logger('dev'));
+app.set('port', APPCONFIG.port);
+app.set('view engine', 'html');
+app.use(express.favicon());
+app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(express.cookieParser(config.cookieSecret));
+app.use(express.cookieParser(APPCONFIG.cookieSecret));
 app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(function(req, res){
+	return res.redirect(req.protocol + '://' + req.get('Host') + '/#' + req.url);
+});
+
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-// mysql setup
-var mysql = require('mysql').createPool({
-	  host: config.mysqlHost
-	, port: config.mysqlPort
-	, user: config.mysqlUser
-	, password: config.mysqlPassword
-	, database: config.mysqlDatabase
-});
-
-mysql.getConnection(function(err,connection){
-	if(err) {
-		console.log('[MYSQL] Connection Pool Failure: ' + err);
-	}
-	else {
-		console.log('[MYSQL] Connection Pool Established');
-	}
-	connection.end();
-});
-
-//app.get('/', routes.index);
-//app.get('/users', user.list);
-
 app.get('/api', function(req, res) {
-	res.send('StickIt API v' + appVer.apiVersion);
+	res.send('StickIt API');
 });
-
-console.log('StickIt API v' + appVer.apiVersion);
-
-var api = require('./controllers')(app, mysql);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('StickIt Server listening on port ' + app.get('port'));
+  console.log('Express server listening on port ' + app.get('port'));
 });
