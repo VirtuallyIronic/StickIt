7b341a1363dcb5484663cf99ced8600a8eef19c0
Evan Scown <sir.lagalot.thegreat@gmail.com>
2013-10-03 07:06:39 +1000

    widget missing dependancies

--- public/js/views/widget/widgetView.js
+++ public/js/views/widget/widgetView.js
@@ -1,5 +1,5 @@
-define([ 'marionette', 'handlebars', 'text!templates/widget/widgetTemplate.html', 'models/CurrentUser', 'settingsmenu'],
-	function (Marionette, Handlebars, template, CurrentUser){  
+define([ 'marionette', 'handlebars', 'text!templates/widget/widgetTemplate.html', 'models/CurrentUser', 'transit', 'settingsmenu'],
+	function (Marionette, Handlebars, template, CurrentUser, Transit, SettingsMenu){  
 		return Marionette.ItemView.extend({
 			template:Handlebars.compile(template),
 			modelEvents:{
