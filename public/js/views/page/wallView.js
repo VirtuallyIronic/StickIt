d97e7d95260955f50cbf6d3682fac623213b964d
Evan Scown <sir.lagalot.thegreat@gmail.com>
2013-10-08 12:01:05 +1000

    testing to view posts 3

--- public/js/views/page/wallView.js
+++ public/js/views/page/wallView.js
@@ -1,13 +1,13 @@
-define([ 'marionette', 'handlebars', 'text!templates/page/wallTemplate.html', 'models/Wall', 'transit', 'settingsmenu'],
-	function (Marionette, Handlebars, template, CurrentWall, Transit, SettingsMenu){  
-		return Marionette.ItemView.extend({
+define([ 'backbone', 'text!templates/page/wallTemplate.html', 'models/Wall', 'transit', 'settingsmenu'],
+	function (Backbone, template, CurrentWall, Transit, SettingsMenu){  
+		return Backbone.View.extend({
 			tag: 'div',
 			template: template,
 			modelEvents:{
 				"change": "render"
 			},
 			onRender: function(){
-				console.log(this.model.attributes.posts);
+				console.log(this.model);
 			}
 		});
 	}
