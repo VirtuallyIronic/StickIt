028acce4b097d988bf5219867a57625d7bffc93e
Evan Scown <sir.lagalot.thegreat@gmail.com>
2013-10-08 10:16:26 +1000

    remove backbone relational

--- public/js/models/Wall.js
+++ public/js/models/Wall.js
@@ -1,4 +1,4 @@
-define(['jquery', 'underscore', 'backbone', 'backbone-relational', 'models/Post'],
+/**define(['jquery', 'underscore', 'backbone', 'backbone-relational', 'models/Post'],
 	function ($, _, Backbone, Relational, modelPost) {
 		var Wall = Backbone.RelationalModel.extend({
 			urlRoot: '/api/wall',
@@ -15,4 +15,14 @@ define(['jquery', 'underscore', 'backbone', 'backbone-relational', 'models/Post'
         });
         return Wall;
     }
+); **/
+
+define(['jquery', 'underscore', 'backbone'],
+	function($, _, Backbone) {
+		var Wall = Backbone.model.extend({
+			urlRoot: '/api/wall',
+			idAttribute: 'id',
+		});
+		return Wall;
+	}
 );
