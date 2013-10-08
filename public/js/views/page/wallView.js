9046d6e763c704fa9f666e2d1f2f37f486cb3b0b
Evan Scown <sir.lagalot.thegreat@gmail.com>
2013-10-08 13:26:27 +1000

    testing to view posts 5

--- public/js/views/page/wallView.js
+++ public/js/views/page/wallView.js
@@ -1,14 +1,12 @@
 define([ 'backbone', 'text!templates/page/wallTemplate.html', 'models/Wall', 'transit', 'settingsmenu'],
 	function (Backbone, template, CurrentWall, Transit, SettingsMenu){  
 		return Backbone.View.extend({
-			tag: 'div',
-			template: template,
+			el: $("#wall"),
 			initialize: function(){
 				this.listenTo(this.model, "change", this.render);
 			},
 			render: function(){
-			    this.$el.html(this.template(this.model.attributes));
-			    return this;
+				this.$el.html(this.template);
 			}
 		});
 	}
