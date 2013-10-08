37e5655adc5ece78ca1a93cbbd4f5071ee6bcec9
Evan Scown <sir.lagalot.thegreat@gmail.com>
2013-10-09 09:24:22 +1000

    item view fix

--- public/js/views/page/wallSettingsView.js
+++ public/js/views/page/wallSettingsView.js
@@ -1,18 +1,9 @@
-define([ 'marionette', 'handlebars', 'json2','text!templates/page/wallPermissionTemplate.html', 'transit', 'settingsmenu', 'utils/permissionsPageController'],
+define([ 'marionette', 'handlebars', 'json2','text!templates/page/wallSettingsTemplate.html', 'transit', 'settingsmenu', 'utils/permissionsPageController'],
 	function (Marionette, Handlebars, json2, template, transit, settingsMenu, permissionsPageController){  
 		return Marionette.ItemView.extend({
 			template:Handlebars.compile(template),
 			modelEvents:{
 				"change": "render"
-			},
-			events: {
-				"click #updateButton": "updatePermissions"
-			},
-			updatePermissions: function(event){
-				event.stopPropagation();
-				event.preventDefault();
-				var checkbox = $('isPrivateCheckbox').val();
-				console.log(checkbox);
 			}
 		});
 	}
