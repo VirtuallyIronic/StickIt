018072229904532be821502adc4004ef1191ee5f
Evan Scown <sir.lagalot.thegreat@gmail.com>
2013-10-08 13:52:47 +1000

    integrate 2

--- public/js/views/page/wallView.js
+++ public/js/views/page/wallView.js
@@ -1,13 +1,7 @@
-define([ 'backbone', 'text!templates/page/wallTemplate.html', 'vendor/jquery.gridster.with-extras', 'utils/ajaxController', 'utils/backbone_gridster_init', 'transit', 'settingsmenu'],
-	function (Backbone, template, gridster, ajaxController, gridsterInit, Transit, SettingsMenu){  
-		return Backbone.View.extend({
-			el: $(".demo"),
-			initialize: function(){
-				this.listenTo(this.model, "change", this.render);
-			},
-			render: function(){
-				this.$el.html(this.template);
-			}
+define([ 'marionette', 'handlebars', 'text!templates/page/wallTemplate.html', 'vendor/jquery.gridster.with-extras', 'utils/ajaxController', 'utils/backbone_gridster_init', 'transit', 'settingsmenu'],
+	function (Marionette, Handlebars, template, gridster, ajaxController, gridsterInit, transit, settingsMenu){  
+		return Marionette.ItemView.extend({
+				template:Handlebars.compile(template)
 		});
 	}
 );
