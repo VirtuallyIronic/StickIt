33a8f510403e1ddd821a929dd27236117564e03d
Evan Scown <sir.lagalot.thegreat@gmail.com>
2013-10-09 10:26:24 +1000

    redirection

--- public/js/utils/ajax_evan.js
+++ public/js/utils/ajax_evan.js
@@ -266,7 +266,8 @@
 	
 	function redirectToSettings(model_data){
 		var url = "/wallsettings/" + model_data.wallId;
-		location.redirect(url);
+		console.log(url);
+		window.location.redirect(url);
 	}
 	
 	function colCreate(model_data)
